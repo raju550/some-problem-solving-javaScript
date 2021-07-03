3003f22112a99dba5a101f638d8a8f420a9e7338
Md Ismail Hossain <ismailtex043@gmail.com>
2021-07-03 23:05:51 +0600

    random

---
+++ random.js
@@ -0,0 +1,13 @@
+var num = 5.899;
+var result = Math.round(num);
+console.log(result);
+var result1 = Math.floor(num);
+console.log(result1);
+var result2 = Math.ceil(num);
+console.log(result2);
+var randomNum = Math.random() * 10;
+console.log(randomNum);
+for (i = 1; i < 10; i++) {
+  var randomNam = Math.random() * 10;
+  console.log(randomNam);
+}
